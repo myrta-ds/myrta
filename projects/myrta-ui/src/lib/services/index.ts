1aa41b1434074e630d4911bbeccf4330572125f5
Velmoren <velmoren@yahoo.com>
2024-03-12 17:48:00 +0300

    npm upd common

--- projects/myrta-ui/src/lib/services/index.ts
+++ projects/myrta-ui/src/lib/services/index.ts
@@ -3,3 +3,5 @@ export * from './file-upload/file-upload.service';
 export * from './mrx-form-validator/mrx-form-validator';
 
 export * from './save-store/save-store.module';
+export * from './save-store/store/selectors';
+export * from './save-store/store/actions';
